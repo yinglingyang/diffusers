3369abe034eb2a255ae47078e32c720cda56c9f9
yinglingyang <89542892+yinglingyang@users.noreply.github.com>
2024-03-25 14:35:29 -0600

    Update train_dreambooth_lora_sdxl.py

--- examples/dreambooth/train_dreambooth_lora_sdxl.py
+++ examples/dreambooth/train_dreambooth_lora_sdxl.py
@@ -1158,7 +1158,7 @@ def main(args):
     # now we will add new LoRA weights to the attention layers
     unet_lora_config = LoraConfig(
         r=args.rank,
-        use_dora=args.use_dora,
+#        use_dora=args.use_dora,
         lora_alpha=args.rank,
         init_lora_weights="gaussian",
         target_modules=["to_k", "to_q", "to_v", "to_out.0"],
@@ -1170,7 +1170,7 @@ def main(args):
     if args.train_text_encoder:
         text_lora_config = LoraConfig(
             r=args.rank,
-            use_dora=args.use_dora,
+#            use_dora=args.use_dora,
             lora_alpha=args.rank,
             init_lora_weights="gaussian",
             target_modules=["q_proj", "k_proj", "v_proj", "out_proj"],
@@ -1927,7 +1927,7 @@ def main(args):
         if args.push_to_hub:
             save_model_card(
                 repo_id,
-                use_dora=args.use_dora,
+#                use_dora=args.use_dora,
                 images=images,
                 base_model=args.pretrained_model_name_or_path,
                 train_text_encoder=args.train_text_encoder,
